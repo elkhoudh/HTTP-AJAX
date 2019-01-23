2e48feb154ac5bdd21245099de2b32e41f7d7a2c
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-23 15:31:23 -0500

    make it better

--- server.js
+++ server.js
@@ -83,9 +83,9 @@ app.delete("/friends/:id", (req, res) => {
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  app.use(express.static("friends/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "friends", "build", "index.html"));
   });
 }
 
